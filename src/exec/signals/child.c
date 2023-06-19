6c3ae9ec902f58a62aa9642a6c5f9c515dccddf9
Daniel González Carretero <dangonza@student.42madrid.com>
2023-06-19 14:40:37 +0200

    Ctrl + \ didn't print 'Quit: 3'

--- src/exec/signals/child.c
+++ src/exec/signals/child.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   child.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: dangonza <dangonza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 19:40:03 by jre-gonz          #+#    #+#             */
-/*   Updated: 2023/06/12 19:40:45 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2023/06/19 14:39:56 by dangonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 static void	sig_handler_child_quit(int signal)
 {
 	(void) signal;
+	printf("Quit: 3\n");
 	rl_redisplay();
 }
 
